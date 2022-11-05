8d4406a337f94837a77d5aff9bf7b6bae2c1602e
Tobias Goetz <tobigoetz98@gmail.com>
2022-11-05 01:30:22 +0100

    Added error message to admin cog

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,6 @@
 import logging
 
+import discord
 from discord.ext import commands
 
 logger = logging.getLogger('discord.admin')
@@ -10,11 +11,19 @@ class Admin(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    @commands.is_owner()
+    @commands.has_permissions(administrator=True)
     async def clear(self, ctx, amount=1):
         await ctx.message.delete()
         await ctx.channel.purge(limit=amount)
         logging.info(f'User {ctx.author} cleared {amount} messages in {ctx.channel}')
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            logger.info(f'User {ctx.author} tried to use {ctx.command} without permissions.')
+            await ctx.send(embed=discord.Embed(
+                description=f':no_entry_sign: **{ctx.author.name}** you cannot use `{ctx.command}`.',
+            ))
 
 
 async def setup(bot):
