c0f0523f989cf56f88c31d6edbdb149ae94342ac
Tobias Goetz <tobigoetz98@gmail.com>
2022-12-05 21:18:37 +0100

    Added mute command to admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,21 +1,79 @@
+"""
+Admin cog
+"""
 import logging
 
+import discord
 from discord.ext import commands
 
 logger = logging.getLogger('discord.admin')
 
 
 class Admin(commands.Cog):
+    """
+    Admin commands
+    """
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def clear(self, ctx, amount=1):
+        """
+        Clear a specified amount of messages.
+        :param ctx: The context of the command.
+        :param amount: The amount of messages to clear.
+        """
         await ctx.message.delete()
         await ctx.channel.purge(limit=amount)
-        logging.info(f'User {ctx.author} cleared {amount} messages in {ctx.channel}')
+        logging.info('User %s cleared %s messages in %s.', ctx.author, amount, ctx.channel)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def mute(self, ctx, member: discord.Member, *, reason=None):
+        """
+        Mute a member.
+        :param ctx: The context of the command.
+        :param member: The member to mute.
+        :param reason: The reason for the mute.
+        """
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+
+        if role is None:
+            logger.info('Muted role not found, creating one.')
+            await ctx.guild.create_role(name='Muted')
+            role = discord.utils.get(ctx.guild.roles, name='Muted')
+
+        for channel in ctx.guild.channels:
+            await channel.set_permissions(
+                role,
+                speak=False,
+                send_messages=False
+            )
+        logger.info('Muted role created.')
+
+        await member.add_roles(role)
+
+        if reason is None:
+            await ctx.send(f'{member.mention} has been muted.')
+        else:
+            await ctx.send(f'{member.mention} has been muted for {reason}')
+        logging.info('User %s muted %s for %s.', ctx.author, member, reason)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def unmute(self, ctx, member: discord.Member):
+        """
+        Unmute a member.
+        :param ctx: The context of the command.
+        :param member: The member to unmute.
+        """
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        await member.remove_roles(role)
+        await ctx.send(f'{member.mention} has been unmuted.')
+        logging.info('User %s unmuted %s.', ctx.author, member)
 
 
 async def setup(bot):
+    """ Setup the cog. """
     await bot.add_cog(Admin(bot))
