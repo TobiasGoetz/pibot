6261e1ef5338918be335dd448511802e36df659e
Tobias Goetz <tobigoetz98@gmail.com>
2022-12-05 11:35:23 +0100

    Fixed pylint issues in general.py

--- cogs/general.py
+++ cogs/general.py
@@ -1,20 +1,32 @@
+"""
+General cog
+"""
 import asyncio
 import logging
 from datetime import datetime, timezone, timedelta
 
 import discord
-import pytimeparse as pytimeparse
+import pytimeparse
 from discord.ext import commands
 
 logger = logging.getLogger('discord.general')
 
 
 class General(commands.Cog):
+    """
+    General commands
+    """
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def countdown(self, ctx, time_str: str):
+        """
+        Start a countdown for a specified amount of time.
+        :param ctx: The context of the command.
+        :param time_str: The time to count down from.
+        :return: None
+        """
         seconds = pytimeparse.parse(time_str)
 
         # Input validation
@@ -27,7 +39,7 @@ class General(commands.Cog):
 
         start_time = ctx.message.created_at
         end_time = (start_time + timedelta(seconds=seconds)).strftime("%H:%M:%S")
-        logger.info(f"{ctx.author} started a countdown for {seconds} seconds.")
+        logger.info("%s started a countdown for %s seconds.", ctx.author, seconds)
         message = await ctx.send(embed=discord.Embed(
             title=f'Countdown - {seconds}s',
             description=(
@@ -39,10 +51,10 @@ class General(commands.Cog):
         while (datetime.now(timezone.utc) - start_time).total_seconds() < seconds:
             await message.edit(embed=discord.Embed(
                 title=f'Countdown - {seconds}s',
-                description=(
-                    f'{seconds - round((datetime.now(timezone.utc) - start_time).total_seconds())} seconds remaining.\n'
-                    f'Ends at {end_time} UTC.'
-                )
+                description=
+                f'{seconds - round((datetime.now(timezone.utc) - start_time).total_seconds())}'
+                f'seconds remaining.\n'
+                f'Ends at {end_time} UTC.'
             ))
             await asyncio.sleep(1)
 
@@ -50,8 +62,12 @@ class General(commands.Cog):
             title=f'Countdown - {seconds}s',
             description=f'Countdown finished at {end_time} UTC.'
         ))
-        logger.info(f"Finished {ctx.author}'s countdown for {seconds} seconds.")
+        logger.info("Finished %s's countdown for %s seconds.", ctx.author, seconds)
 
 
 async def setup(bot):
+    """
+    :param bot: The bot to add the cog to.
+    :return: None
+    """
     await bot.add_cog(General(bot))
