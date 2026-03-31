d901cbf1f3ccc0d93a511fb74f888ae707e64219
Tobias Goetz <contact@tobiasgoetz.com>
2026-03-31 13:43:05 +0200

    fix: subpackages

--- src/pibot/translation_service/__init__.py
+++ src/pibot/translation_service/__init__.py
@@ -1,6 +1,1 @@
 """Translation service package."""
-
-from pibot.translation_service.deepl_translator import DeepLTranslator
-from pibot.translation_service.translator import Translator
-
-__all__ = ["DeepLTranslator", "Translator"]
